e73279982db875ddfe138bb3f75902d93c9b37b2
Giovanni Andrade <giovanni.andrade9991@gmail.com>
2020-11-24 15:06:54 -0300

    Interface for housing, fixed some other bugs

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { IProperty } from '../property-list/IProperty.interface';
 
 @Component({
   selector: 'app-property-card',
@@ -6,10 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./property-card.component.scss']
 })
 export class PropertyCardComponent {
-  @Input() property : any
- 
-  Property: any;
-
+  @Input() property : IProperty;
   
 
   constructor() { }
--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { IProperty } from '../property/property-list/IProperty.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class HousingService {
   getAllProperties(){
     return this.http.get('data/properties.json').pipe(
       map(data => {
-        const propertiesArray: Array<any> = [];
+        const propertiesArray: Array<IProperty> = [];
         for (const id in data){
           if(data.hasOwnProperty(id)){
             propertiesArray.push(data[id]);
