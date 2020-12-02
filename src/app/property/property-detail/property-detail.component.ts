94b83273b1d463e5c50efb7ed99483503d0c1353
Giovanni Andrade <giovanni.andrade9991@gmail.com>
2020-12-02 18:26:03 -0300

    Routing fixes

--- src/app/property/property-detail/property-detail.component.ts
+++ src/app/property/property-detail/property-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-property-detail',
@@ -10,10 +10,14 @@ export class PropertyDetailComponent implements OnInit {
 
   public propertyId: number;
 
-    constructor(private route: ActivatedRoute) { }
+    constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-     this.propertyId = this.route.snapshot.params['id']
+     this.propertyId = Number(this.route.snapshot.params['id']);
   }
 
+  onSelectProx() { 
+    this.propertyId += 1;
+    this.router.navigate(['property-detail', this.propertyId])
+  }
 }
